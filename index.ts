120a3c5e17ac92a71cc4712295295b5b79130473
Taras Khmelovskyi <khmelovski@gmail.com>
2022-06-25 14:39:33 +0300

    feat: add ports with .env

--- index.ts
+++ index.ts
@@ -1,17 +1,27 @@
+import dotenv from 'dotenv';
 import Jimp from 'jimp';
 import { httpServer } from './src/http_server/index';
 import robot from 'robotjs';
-import { createWebSocketStream, WebSocketServer } from 'ws';
+import WebSocket, { WebSocketServer } from 'ws';
 
-const HTTP_PORT = 3000;
+dotenv.config();
+
+const HTTP_PORT: number = Number(process.env.FRONT_PORT) || 3000;
+const WSS_PORT: number = Number(process.env.WSS_PORT) || 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
-const wss = new WebSocketServer({ port: 8080 });
+const wss = new WebSocketServer({ port: WSS_PORT });
 
-wss.on('connection', (ws) => {
-	ws.on('message', (data) => {
-		console.log(data);
+wss.on('connection', (ws: WebSocket) => {
+	console.log('WS Client connected');
+	ws.on('message', (message: string) => {
+		console.log('received: %s', message);
 	});
+	ws.send('something');
+});
+
+wss.on('close', (): void => {
+	console.log('WS Client disconnected');
 });
